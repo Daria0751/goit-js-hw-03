adaf077af03897b7bceda4149af40191893dfe42
Daria <darak0751@gmail.com>
2025-02-02 19:48:32 +0200

    hw3

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+  return title.trim().toLowerCase().replace(/\s+/g, '-');
+}
+
+
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
